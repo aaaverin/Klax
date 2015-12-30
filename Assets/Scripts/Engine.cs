304cb747e1eb5a96d571bb9afcff469a32752889
Aleksey Averin <aleaverin@gmail.com>
2015-12-30 12:16:48 +0300

    engine template

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using Klax.World;
+using Klax.Common;
+
+namespace Klax.Core
+{
+	public class Engine : MonoBehaviour 
+	{
+		[SerializeField]
+		BlockPool blocks;
+		[SerializeField]
+		Vector3[] respawns;
+
+		Grid grid;
+
+		void Start () 
+		{
+			grid = new Grid (() => Debug.Log ("Game Over"), (x,y) => Debug.Log (x + ":" + y));
+		}
+		
+		void StartRandom()
+		{
+			var randomLine = Random.Range (0, respawns.Length);
+			var block = blocks.GetRandom ();
+			var blockTransform = block.Transform;
+			blockTransform.position = respawns [randomLine];
+			block.StartMoving();
+		}
+
+		void Drop(int line, Block block)
+		{
+			grid.DropTo (line, block.Color, () => blocks.Return (block));
+		}
+	}
+}
