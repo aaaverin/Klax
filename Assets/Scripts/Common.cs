8f8936f8eb59d2a9edf30efdd1750f7816ee838e
Aleksey Averin <aleaverin@gmail.com>
2015-12-30 12:12:26 +0300

    common code

---
+++ Assets/Scripts/Common.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Klax.Common
+{
+	public delegate void WinLineDelegate(LineType Line, int blockCount);
+
+	public static class BlockColorExtention
+	{
+		public static Color ToUnityColor(this BlockColor color)
+		{
+			switch (color)
+			{
+			case BlockColor.Black:
+				return Color.black;
+			case BlockColor.Blue:
+				return Color.blue;
+			default:
+				return Color.white;
+			}
+		}
+	}
+
+	public struct Point
+	{
+		public int x;
+		public int y;
+		
+		public Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public enum LineType
+	{
+		MainDiagonal,
+		Vertical,
+		SecondaryDiagonal,
+		Horizontal
+	}
+
+	public enum BlockColor
+	{
+		Black,
+		Blue,
+		Green,
+		Yellow,
+		Red,
+		Cyan,
+		Magenta,
+		White
+	}
+}
